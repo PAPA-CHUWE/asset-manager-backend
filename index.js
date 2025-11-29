0f55b46c7822b77a69acade157c8dede884b46e0
Terrence Chuwe <your.email@example.com>
2025-11-29 21:57:20 +0200

    cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import postgres from 'postgres';
 import dotenv from 'dotenv';
+import cors from 'cors'; 
 import AdminAuth from './adminAuth/AdminAuth.js'; // Your auth routes
 import { createAdminUsers } from './utils/GenerateAdmin.js';
 
@@ -12,6 +13,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON
 app.use(express.json());
+
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Connect to Supabase Postgres
 const sql = postgres(process.env.DATABASE_URL, {
